12c0679a5268da27dd7978dce2a62f09f312573c
kh-naman <namankhandelwalss@gmail.com>
2025-06-08 11:45:51 +0530

    Init inngest instance and signup pipeline

---
+++ ai-ticket-agent/inngest/functions/on-signup.js
@@ -0,0 +1,38 @@
+import { NonRetriableError } from 'inngest'
+import user from '../../models/user.js'
+import {inngest} from '../client.js'
+import { sendMail } from '../../models/utils/mailer.js'
+
+inngest.createFunction(
+    { id: "on-signup" },
+    { event: "user/signup" },
+    async ({event,step}) => {
+        try {
+            const {email} = event.data
+            const user = await step.run("get-user-from-email",async() => {
+                const userObject = user.findOne({email})
+                if(!userObject)
+                {
+                    throw new NonRetriableError("User no longer exist in our db")
+                }
+
+                return userObject
+            })
+
+            await step.run("send-signup-email", async()=>{
+                const subject = `Welcome to the AI-Driven ticketing system`
+                const message = `Hello,
+                \n\n
+                Thanks for signing up, ready to unleash the AI-driven ticketing system?!!
+                `
+                await sendMail(user.email,subject,message)
+            })
+
+            return {succes: true}
+        } catch (error) {
+            console.error("❌ Error running step",error.message);
+            return {succes: false}
+            
+        }
+    }
+)
