c0d2031716a1c4be573bb531cfd5502ad31c6406
kh-naman <namankhandelwalss@gmail.com>
2025-06-08 14:56:47 +0530

    User related controllers added

---
+++ ai-ticket-agent/controllers/user.js
@@ -0,0 +1,116 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/user"
+import {inngest} from "../inngest/client.js"
+
+export const signup = async (req,res) => {
+    const {email,password,skills = []} = req.body
+    try {
+        const hashedPassword = bcrypt.hash(password,10)
+        const user = await User.create({
+            email,password:hashedPassword,skills
+        })
+
+        await inngest.send({
+            name: "user/signup",
+            data: {
+                email
+            }
+        })
+
+       const token =  jwt.sign(
+            {_id: user._id,role:user.role},
+            process.env.JWT_SECRET
+        )
+
+        res.json({user,token})
+    } catch (error) {
+        res.status(500).json({error: "Signup failed",
+            details: error.message
+            }
+        )
+    }
+}
+
+export const login = async (req,res) =>{
+    const {email,password} = req.body
+
+    try {
+        const userObject = User.findOne({email})
+
+        if(!userObject)
+        {
+            return res.status(401).json({error: "User not found"})
+        }
+
+        const isPasswordMatch = bcrypt.compare(password,userObject.password)
+
+        if(!isPasswordMatch)
+        {
+            return res.status(401).json({error: "Invalid credentials" })
+        }
+
+
+    } catch (error) {
+        res.status(500).json({error: "Login failed",
+            details: error.message
+            }
+        )
+    }
+    
+
+
+}
+
+export const logout = async (req,res) =>{
+    const token = req.headers.authorization.split(" ")[1]
+    if(!token)  return res.status(401).json({error: "Unauthorized"})
+    jwt.verify(token,process.env.JWT_SECRET, (err,decoded) => {
+        if(err) return res.status(401).json({error: "Unauthorized"})
+        res.json({message: "Logout successfully"})
+    })
+}
+
+
+export const updateUser = async (req,res) => {
+    const {skills =[], role, email} = req.body
+
+    try {
+        
+        if(req.user?.role !== "admin")
+        {
+            return res.status(403).json({error: "Forbidden"})
+        }
+
+        const user = await User.findOne({email})
+
+        if(!user)
+        {
+            return res.status(401).json({error: "User not found"})
+        }
+
+        await User.updateOne(
+            {email},
+            {skills: skills.length ? skills: user.skills }
+        )
+
+        return res.json({message: "User updated successfuly"})
+    } catch (error) {
+           return res.status(500).json({error: "Updation failed",details: error.message})
+    }
+
+}
+
+export const getUsers = async (req,res) => {
+    try {
+        if(req.user.role !== "admin")
+        {
+            return res.status(403).json({error: "Forbidden"})
+        }
+
+        const users = User.find().select("-password")
+        return res.json(users)
+    } catch (error) {
+        return res.status(500).json({error: "Getting users failed",details: error.message})
+    }
+}
