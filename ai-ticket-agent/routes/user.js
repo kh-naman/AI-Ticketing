84a8e630249776a953e66e24b636f019ddf9134f
kh-naman <namankhandelwalss@gmail.com>
2025-06-08 15:18:10 +0530

    User route and dummy ticket route added

---
+++ ai-ticket-agent/routes/user.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { signup,login,logout,updateUser,getUsers } from '../controllers/user.js'
+import { authenticate } from '../middlewares/auth.js'
+
+const router = express.Router()
+
+router.post("/signup",signup)
+
+router.post("/login",login)
+
+router.post("/logout",logout)
+
+router.post("/update-user",authenticate,updateUser)
+
+router.post("/users",authenticate,getUsers)
+
+export default router
