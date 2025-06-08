699b7121fe19893015d896430ee8241f769234dc
kh-naman <namankhandelwalss@gmail.com>
2025-06-08 15:17:43 +0530

    Import correction

--- ai-ticket-agent/index.js
+++ ai-ticket-agent/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import userRoutes from "./routes/user.js"
 
 
 
@@ -10,7 +11,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 dotenv.config();
+
+app.use("/api/users",userRoutes)
 
 mongoose
         .connect(process.env.MONGO_URI)
