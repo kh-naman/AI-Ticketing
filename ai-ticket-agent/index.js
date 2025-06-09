6b3051257b93f74360845cf30b468cfb80fd5af4
kh-naman <namankhandelwalss@gmail.com>
2025-06-09 20:13:36 +0530

    Setting up inngest middleware server

--- ai-ticket-agent/index.js
+++ ai-ticket-agent/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import {serve} from "inngest/express"
 import dotenv from 'dotenv'
 import userRoutes from "./routes/user.js"
+import ticketRoutes from "./routes/ticket.js"
+import { inngest } from './inngest/client.js'
+import { onUserSignup } from './inngest/functions/on-signup.js'
+import { onTicketCreated } from './inngest/functions/on-ticket-create.js'
 
 
 
@@ -15,6 +20,12 @@ app.use(express.json())
 dotenv.config();
 
 app.use("/api/auth",userRoutes)
+app.use("/api/tickets",ticketRoutes)
+
+app.use("/api/inngest",serve({
+    client: inngest,
+    functions: [onUserSignup,onTicketCreated]
+}))
 
 mongoose
         .connect(process.env.MONGO_URI)
