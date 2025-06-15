f1b4f67259e3a1da5a94b152684d172c47314d51
kh-naman <namankhandelwalss@gmail.com>
2025-06-15 09:35:12 +0530

    Adding nav bar component

---
+++ ai-ticket-frontend/src/components/navbar.jsx
@@ -0,0 +1,54 @@
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Navbar() {
+  const token = localStorage.getItem("token");
+  let user = null;
+const userStr = localStorage.getItem("user");
+if (userStr && userStr !== "undefined") {
+  try {
+    user = JSON.parse(userStr);
+  } catch {
+    user = null; // fallback if parsing fails
+  }
+}
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+  return (
+    <div className="navbar bg-base-200">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">
+          Ticket AI
+        </Link>
+      </div>
+      <div className="flex gap-2">
+        {!token ? (
+          <>
+            <Link to="/signup" className="btn btn-sm">
+              Signup
+            </Link>
+            <Link to="/login" className="btn btn-sm">
+              Login
+            </Link>
+          </>
+        ) : (
+          <>
+            <p>Hi, {user?.email}</p>
+            {user && user?.role === "admin" ? (
+              <Link to="/admin" className="btn btn-sm">
+                Admin
+              </Link>
+            ) : null}
+            <button onClick={logout} className="btn btn-sm">
+              Logout
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
